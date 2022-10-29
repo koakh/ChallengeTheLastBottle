9a0344c7466710a591b1ef94d94d5dc5123672e8
Mario Monteiro <marioammonteiro@gmail.com>
2022-10-29 17:03:15 +0100

    minor changes on Dockerfile

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
     // override default here
-    // port: 5173
+    port: 5173
   }
 })
